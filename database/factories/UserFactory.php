70b4f9c1e44a7b008dd25a07353b6e7e8f9b1fc1
Scaff <thibault.roussel@gmail.com>
2024-03-29 11:25:19 +0100

    refacto: Supprimer le code inutilisé dans UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,19 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
