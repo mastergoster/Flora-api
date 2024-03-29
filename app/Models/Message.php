5ef40cb3d82909a68d88305c7e0ff96026899c31
Scaff <thibault.roussel@gmail.com>
2024-03-29 11:20:56 +0100

    chore: Ajouter le modèle Message et la fabrique de messages

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,8 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
     use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'message',
+        'to_user_id',
+        'to_role_id',
+    ];
+
+    /**
+     * Get the user that the message is to.
+     */
+    public function toUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+
+    /**
+     * Get the role that the message is to.
+     */
+    public function toRole(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'to_role_id');
+    }
 }
