1a99a3cca84dd565d89e0af9319d6f54cbccb51c
Scaff <thibault.roussel@gmail.com>
2024-03-29 10:57:05 +0100

    chore: Compléter le modèle AttendanceLog et la fabrique AttendanceLogFactory

--- app/Models/AttendanceLog.php
+++ app/Models/AttendanceLog.php
@@ -5,8 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AttendanceLog extends Model
 {
     use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'reservable_items_id',
+    ];
+
+    /**
+     * Get the user that owns the attendance log.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the reservable item that the attendance log belongs to.
+     */
+    public function reservableItem(): BelongsTo
+    {
+        return $this->belongsTo(ReservableItem::class, 'reservable_items_id');
+    }
 }
